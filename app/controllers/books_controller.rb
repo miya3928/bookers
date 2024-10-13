d1787169c347694b8394b5de493e289260746751
TatsuyaMiyagawa <181440097+miya3928@users.noreply.github.com>
2024-10-13 05:34:57 +0000

    edit,show完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,21 +4,46 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new
-    book.save
-    redirect_to '/top'
-   
+   @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book), notice: "Book was successfully created."
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
+  def index
+    @book = Book.new 
+    @books = Book.all
+  end
+  
   def show
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+   if @book.update(book_params)
+    redirect_to book_path(@book), notice: "Book was successfully updated."
+   else
+    render:edit
+   end
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def destroy
+    book = Book.find(params[:id]) 
+    book.destroy  
+    redirect_to books_path(@book) , notice: "Book was successfully deleted." 
   end
   
   private
   
   def book_params
-    params.require(:list).permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
   config.hosts << "aa9e42ba56a1427f987ce6e44e9bdf82.vfs.cloud9.ap-northeast-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.host.clear
 end
